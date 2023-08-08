640268d7b83360f2e0d4765c968bba80fbf1f94e
ashish singh <ashsixtysix@gmail.com>
2023-08-08 20:20:22 +0530

    some comments added

--- Stock_Market_Web_App/stock/quotes/urls.py
+++ Stock_Market_Web_App/stock/quotes/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
 	path('news.html', views.news, name="news"),
     path('learning.html', views.learning, name="learning"),
     path('watchlist.html', views.watchlist, name="watchlist"),
-	path('candlestick-chart-data/', views.candlestick_chart, name='candlestick-chart'),
-    path('candlestick-chart/',views.candlestick_chart_data, name='candlestick-chart-data'),
-    #TSLA
-	path('candlestick-chart-tsla/', views.candlestick_chart_data_tsla,name='candlestick-chart-data-tsla'),
+	path('candlestick-chart-data/', views.candlestick_chart, name='candlestick-chart'),			#path to show both charts
+    path('candlestick-chart/',views.candlestick_chart_data, name='candlestick-chart-data'),			#path to fetch historical data of AAPL   
+	path('candlestick-chart-tsla/', views.candlestick_chart_data_tsla,
+	     name='candlestick-chart-data-tsla'),  # path to fetch historical data of TSLA
 ]
